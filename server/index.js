ba1b783facb07b293cc1f808eb0e313ed98f9332
Tahmidbugs <tahmidahmed1@usf.edu>
2022-12-16 20:38:34 -0500

    CRUD operations using Postgres

--- server/index.js
+++ server/index.js
@@ -1,10 +1,76 @@
 const express = require("express");
 const app = express();
 const cors = require("cors"); // helps client and server in different ports to interact together
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 app.use(express.json()); // gives access to req.body
+
+//ROUTES
+//Create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newToDo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newToDo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//Get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todo");
+    res.json(todos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id=$1", [
+      req.params.id,
+    ]);
+    res.json(todo.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateToDo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("todowas updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteToDo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("todowas deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
